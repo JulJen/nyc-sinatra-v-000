de675b12e689ad1ac2af2367f4865b4c2728cb4e
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 01:06:23 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -15,17 +15,17 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
+    @landmark = Landmark.find_by(params[:id])
     erb :'/landmarks/show'
   end
 
   get '/landmarks/:id/edit' do
-    @landmark = Landmark.find_by_id(params[:id])
+    @landmark = Landmark.find_by(params[:id])
     erb :'/landmarks/edit'
   end
 
   patch '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
+    @landmark = Landmark.find_by(params[:id])
     @landmark.update(params[:landmark])
 
     @landmark.save
