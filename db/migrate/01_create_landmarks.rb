b58f2c1ea1dea6eab67d503c80267653712be1cc
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 02:29:58 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,4 +1,4 @@
-class CreateLandmarks <ActiveRecord::Migration[5.1]
+class CreateLandmarks < ActiveRecord::Migration[5.1]
   # raise "Write CreateLandmarks migration here"
   def change
     create_table :landmarks do |t|
